2092330015508592d5cc13e69f7fc9c033867816
jrees4 <44977311+jrees4@users.noreply.github.com>
2022-02-01 13:13:23 +0000

    Add files via upload

---
+++ www/Contact.php
@@ -0,0 +1,12 @@
+<div id="projects"> 
+        <!-- Anything in this tag below is php code -->
+        <?php
+            
+           $page = 'Contact';
+
+            echo("");
+            
+           
+        ?> 
+        <p>You can email me on weekdays from 10am-5pm using Rees.Jay@Outlook.com</p>
+        </div>
